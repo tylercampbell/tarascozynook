0bf536a6970fe9fb7d80315c5765a6c81ccae6fd
tylercampbell <tylercampbell@users.noreply.github.com>
2024-07-11 22:35:11 -0400

    add sitemap & seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: 'https://tarascozynook.com',
+  integrations: [tailwind(), sitemap()]
 });
