d712701c7b813cfa2e4c1418e044d1ebe8348c3b
tylercampbell <tylercampbell@users.noreply.github.com>
2024-07-11 22:47:42 -0400

    add inline

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-
 import playformCompress from "@playform/compress";
+
+import playformInline from "@playform/inline";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tarascozynook.com',
-  integrations: [tailwind(), sitemap(), playformCompress()]
+  integrations: [tailwind(), sitemap(), playformInline(), playformCompress()]
 });
