3a15ab4baa185e7274885b0ed3fb21ea121d0905
tylercampbell <tylercampbell@users.noreply.github.com>
2024-07-11 22:44:11 -0400

    add compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import sitemap from "@astrojs/sitemap";
+
+import playformCompress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://tarascozynook.com',
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap(), playformCompress()]
 });
