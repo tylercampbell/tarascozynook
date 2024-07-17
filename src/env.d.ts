7ce7fddc569dde0b196a8ea0acfb1c94f6c293f1
tylercampbell <tylercampbell@users.noreply.github.com>
2024-07-17 19:29:33 -0400

    button colors

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,9 +1,1 @@
 /// <reference types="astro/client" />
-
-// export {};
-
-// declare global {
-//   interface Window {
-//     adjustFormPositionTimeout: ReturnType<typeof setTimeout>;
-//   }
-// }
