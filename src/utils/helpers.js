9acb954102389f4bc992a05ea94e1434c22d1505
Tyler Campbell <tylercampbell@users.noreply.github.com>
2025-04-24 15:14:15 -0400

    styling, new covers

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -4,10 +4,10 @@ export function getColors() {
   return colorsData;
 }
 
-export function getBooksByNewStatus(offerings, isNew) {
+export function getBooksByFeaturedStatus(offerings, isFeatured) {
   return Object.keys(offerings).flatMap(categoryKey => {
     return Object.keys(offerings[categoryKey].series).flatMap(seriesKey => {
-      return offerings[categoryKey].series[seriesKey].books.filter(book => book.new === isNew);
+      return offerings[categoryKey].series[seriesKey].books.filter(book => book.featured === isFeatured);
     });
   });
 }
